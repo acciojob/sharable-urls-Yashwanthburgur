37c36bce29b9c56ed9f0081357849aff616cfd63
Yashwanth M <byg2281999@gmail.com>
2025-11-26 22:20:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// your code here
+document.getElementById('button').addEventListener('click', function() {
+    const name = document.getElementById('name').value;
+    const year = document.getElementById('year').value;
+    let url = 'https://localhost:8080/';
+    
+    if (name || year) {
+        url += '?';
+        if (name) url += `name=${name}`;
+        if (name && year) url += '&';
+        if (year) url += `year=${year}`;
+    }
+    
+    document.getElementById('url').textContent = url;
+});
